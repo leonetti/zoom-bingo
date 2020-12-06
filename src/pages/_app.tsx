48f8b248d471f8d56aa048668270c48e6d007645
Alex Leonetti <alexleonetti.dev@gmail.com>
2020-12-06 00:48:20 -0800

    Integrated Material UI
    Added simple theme
    Added component
    udpated _app and _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,34 @@
 // This file functions as a main layout for all pages
 import '../styles/globals.scss';
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import GlobalNavigation from '../components/globalNavigation';
+import theme from '../styles/theme';
 
-export default function App({ Component, pageProps }: any) {
+export default function MyApp(props: any) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
   return (
-    <main className="global_wrapper">
-      <GlobalNavigation />
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>Zoom Games</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <GlobalNavigation />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,5 +1,50 @@
-export default function games() {
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+interface GameList {
+  list: Game[]
+}
+
+interface Game {
+  id: number,
+  name: string;
+  rules: string;
+}
+
+export default function games({ list }: GameList) {
   return (
-    <h1>This is a games page</h1>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Rules</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {list.map((row: Game) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.rules}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
+
+games.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/games');
+  const json = await res.json();
+  return { list: json };
+};
